6c2232a75d9ce01190b97c0227518022031051ea
SSubPPaRR <55339796+SSubPPaRR@users.noreply.github.com>
2021-02-03 22:44:47 -0400

    -Modified: new search method loads in books on scroll

--- lib/Widgets/CatalogSearchResult.dart
+++ lib/Widgets/CatalogSearchResult.dart
@@ -1,4 +1,4 @@
-import 'package:bookzapp/Widgets/CatalogSearchList.dart';
+import 'package:bookzapp/Widgets/CatalogSearchResultGrid.dart';
 import 'package:bookzapp/model/BookSet.dart';
 import 'package:bookzapp/model/Utilities.dart';
 import 'package:flutter/cupertino.dart';
@@ -15,19 +15,28 @@ class CatalogSearchResult extends StatefulWidget {
 }
 
 class _CatalogSearchResultState extends State<CatalogSearchResult> {
-  Future<BookSet> retrieveSearchedBooks() async {
+  int maxPages;
+  int latestPage = 1;
+
+  Future<BookSet> retrieveSearchedBooks(int currentPage) async {
     var bookSet = await Utilities.fetchBookSet(
-        "https://api.itbook.store/1.0/search/" + widget.query);
+        "https://api.itbook.store/1.0/search/" +
+            widget.query +
+            "/$currentPage");
     return bookSet;
   }
 
-  Future<void> refresh() async {
-    setState(() {
-      retrieveSearchedBooks();
-    });
+  void setMaxPages() async {
+    maxPages = ((await retrieveSearchedBooks(1)).total / 10).round();
+    print('maxPages: $maxPages');
   }
 
   @override
+  void initState() {
+    setMaxPages();
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     // calculate height for search result widget
     final bodyHeight = MediaQuery.of(context).size.height -
@@ -37,49 +46,55 @@ class _CatalogSearchResultState extends State<CatalogSearchResult> {
 
     print("SQ: " + widget.query);
     print("SO: " + widget.sortOption.toString());
+
     // if there is nothing being searched
     if (widget.query == "") {
       return Center(child: Text('Please enter a search query'));
     } else {
       //get searched books
-      return FutureBuilder(
-          future: retrieveSearchedBooks(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            } else if (snapshot.data.error == -1) {
-              return RefreshIndicator(
-                  child: NotificationListener<OverscrollIndicatorNotification>(
-                      onNotification:
-                          (OverscrollIndicatorNotification overScroll) {
-                        overScroll.disallowGlow();
-                        return true;
-                      },
-                      child: SingleChildScrollView(
-                          physics: AlwaysScrollableScrollPhysics(),
-                          child: Container(
-                              alignment: Alignment.center,
-                              color: Colors.red,
-                              height: bodyHeight,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  Icon(
-                                    Icons.signal_wifi_off,
-                                    size: 50,
-                                  ),
-                                  Text('No connection, reload to try again'),
-                                ],
-                              )))),
-                  onRefresh: () => refresh());
-            } else
-              return RefreshIndicator(
-                //todo: add ability to load more search results
-                child: CatalogSearchList(Utilities.sortBookList(
-                    widget.sortOption, snapshot.data.books)),
-                onRefresh: () => refresh(),
-              );
-          });
+      return Container(
+        color: Colors.deepOrange,
+        child: ListView.builder(
+            addAutomaticKeepAlives: true,
+            itemCount: maxPages,
+            itemBuilder: (context, index) {
+              if (latestPage < index + 1) {
+                latestPage = index + 1;
+              }
+              return FutureBuilder(
+                  future: retrieveSearchedBooks(index + 1),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return SizedBox(
+                          height: MediaQuery.of(context).size.height * 2,
+                          child: Align(
+                              alignment: Alignment.topCenter,
+                              child: CircularProgressIndicator()));
+                    }
+                    // this part might need to be reworked
+                    else if (snapshot.data.error == -1) {
+                      return Container(
+                          alignment: Alignment.center,
+                          color: Colors.red,
+                          height: bodyHeight,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.signal_wifi_off,
+                                size: 50,
+                              ),
+                              Text('No connection, reload to try again'),
+                            ],
+                          ));
+                    } else {
+                      print('latest page: $latestPage');
+                      //grid items here
+                      return CatalogSearchResultGrid(snapshot.data.books);
+                    }
+                  });
+            }),
+      );
     }
   }
 }
